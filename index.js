4c18ddc7379e7ef493c0a8be14c0e94da5c03b3f
JacquelineMalatare <jacqueline.malatare@gmail.com>
2022-08-23 10:12:38 -0600

    update

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let first = 0; first < array.length; first++) {
+    for (let second = first+1; second < array.length; second++) {
+      if(array[first]+array[second] == target){
+        return true
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  For (first) O(n)
+    For (second) O(n)
+      O(1)
+  O(1)
+  Result O(n2) bc we have 2 nested fors
 */
 
 /* 
-  Add your pseudocode here
+  iterate array stating with the first element
+    iterate array starting with the element after the first one selected before
+      check if the sum of both is equal to the target
+        return true if statement is true
+  
+  Return false if the if statement was never true
 */
 
 /*
-  Add written explanation of your solution here
+  To find a pair that is equal to the target, we must verified one by one the sum of each number. if we find a valid sum we return true if not we return false.
 */
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
